2708e46058d90b5b8007b229a6d1e985e9ecb2de
Abdul Razak <moabdulrazak17@gmail.com>
2025-01-13 00:13:32 +0000

    feat: more tip extensions
    task list, image, image resize

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,18 +1,38 @@
 'use client';
 
-import { useEditor, EditorContent } from '@tiptap/react';
+import Image from '@tiptap/extension-image';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import ImageResize from 'tiptap-extension-resize-image';
 
 export const Editor = () => {
 	const editor = useEditor({
 		editorProps: {
 			attributes: {
-        style: 'padding-left: 56px; padding-right: 56px;',
+				style: 'padding-left: 56px; padding-right: 56px;',
 				class:
 					'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
 			},
 		},
-		extensions: [StarterKit],
+		extensions: [
+			StarterKit,
+			TaskItem.configure({
+				nested: true,
+			}),
+			TaskList,
+			Table,
+			TableCell,
+			TableHeader,
+			TableRow,
+			Image,
+			ImageResize,
+		],
 		content: 'Hello, world!',
 	});
 	return (
