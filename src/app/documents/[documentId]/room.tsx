78aead2c0c1d46f2104c3f8a2df0c471e68e0863
Abdul Razak <moabdulrazak17@gmail.com>
2025-02-24 21:49:13 +0000

    feat: get collaborators info

--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -11,17 +11,69 @@ import { useParams } from 'next/navigation';
 import { ReactNode, useEffect, useMemo, useState } from 'react';
 import { toast } from 'sonner';
 import { Id } from '../../../../convex/_generated/dataModel';
+import { getDocuments, getUsers } from './actions';
 
 type User = { id: string; name: string; avatar: string; color: string };
 
 export function Room({ children }: { children: ReactNode }) {
 	const params = useParams();
 
+	const [users, setUsers] = useState<User[]>([]);
+
+	const fetchUsers = useMemo(
+		() => async () => {
+			try {
+				const list = await getUsers();
+				setUsers(list);
+			} catch {
+				toast.error('Failed to fetch users');
+			}
+		},
+		[]
+	);
+
+	useEffect(() => {
+		fetchUsers();
+	}, [fetchUsers]);
+
 	return (
 		<LiveblocksProvider
-			publicApiKey={
-				'pk_dev_AhEnUROWF5mCWzi3E3KrnTt4xa1US4Fk6RLUk7j9jC0UdNdgDlt3svVxl6h2COf4'
-			}
+			throttle={16}
+			authEndpoint={async () => {
+				const endpoint = '/api/liveblocks-auth';
+				const room = params.documentId as string;
+				console.warn('DEBUGPRINT[403]: room.tsx:48: room=', room);
+
+				const response = await fetch(endpoint, {
+					method: 'POST',
+					body: JSON.stringify({ room }),
+				});
+
+				return await response.json();
+			}}
+			resolveUsers={({ userIds }) => {
+				return userIds.map(
+					(userId) => users.find((user) => user.id === userId) ?? undefined
+				);
+			}}
+			resolveMentionSuggestions={({ text }) => {
+				let filteredUsers = users;
+
+				if (text) {
+					filteredUsers = users.filter((user) =>
+						user.name.toLowerCase().includes(text.toLowerCase())
+					);
+				}
+
+				return filteredUsers.map((user) => user.id);
+			}}
+			resolveRoomsInfo={async ({ roomIds }) => {
+				const documents = await getDocuments(roomIds as Id<'documents'>[]);
+				return documents.map((document) => ({
+					id: document.id,
+					name: document.name,
+				}));
+			}}
 		>
 			<RoomProvider
 				id={params.documentId as string}
