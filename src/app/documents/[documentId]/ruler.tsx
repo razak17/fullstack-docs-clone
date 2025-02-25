3be04a76c40c98711c7a8a542bde3d44c6127bbd
Abdul Razak <moabdulrazak17@gmail.com>
2025-02-25 17:05:30 +0000

    feat: functional margins

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { FaCaretDown } from 'react-icons/fa';
+import { useStorage, useMutation } from '@liveblocks/react';
 import { RIGHT_MARGIN_DEFAULT, LEFT_MARGIN_DEFAULT } from '@/constants/margins';
 
 type MarkerProps = {
@@ -42,9 +43,17 @@ const Marker = ({
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 export const Ruler = () => {
-	const [leftMargin, setLeftMargin] = useState(LEFT_MARGIN_DEFAULT);
+	const leftMargin =
+		useStorage((root) => root.leftMargin) ?? LEFT_MARGIN_DEFAULT;
+	const setLeftMargin = useMutation(({ storage }, position: number) => {
+		storage.set('leftMargin', position);
+	}, []);
 
-	const [rightMargin, setRightMargin] = useState(RIGHT_MARGIN_DEFAULT);
+	const rightMargin =
+		useStorage((root) => root.rightMargin) ?? RIGHT_MARGIN_DEFAULT;
+	const setRightMargin = useMutation(({ storage }, position: number) => {
+		storage.set('rightMargin', position);
+	}, []);
 
 	const [isDraggingLeft, setIsDraggingLeft] = useState(false);
 	const [isDraggingRight, setIsDraggingRight] = useState(false);
